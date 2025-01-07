e7b9a296ae3dda7966273b93045c3a09a536eb5c
Harshit <harshitdutttyagi@gmail.com>
2025-01-07 21:56:12 +0530

    first commit

---
+++ client/src/routes/homepage/Homepage.jsx
@@ -0,0 +1,82 @@
+
+import { Link } from "react-router-dom";
+import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
+const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
+  return (
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+            Hey Marketers this is your one stop solution for Generating ATS friendly Resumes.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Human:Help me in Creation of the Resume",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:Sure! Give me Job Description to Analyze",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2:This is the JD for a senior Developer....",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:Here is your content according the project for Senior Developer.",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
